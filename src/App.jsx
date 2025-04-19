bbdcd9dd81e3bd230ad22ffc4d54d27bb3184746
kiranchavan <125879292+Kiranchavan05@users.noreply.github.com>
2025-04-19 16:52:08 +0530

    updated link

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer, toast } from 'react-toastify';
 
 
 const App = () => {
-  const url="http://localhost:4000"
+  const url="https://revamp-maakakhana-backend-2.onrender.com"
   return (
     <div>
       <ToastContainer />
